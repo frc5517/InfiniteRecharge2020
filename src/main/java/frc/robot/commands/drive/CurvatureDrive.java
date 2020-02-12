93ced7ebcc35cae79d7e1343690e5576cf749fee
Jesse Estes <jestes5111@gmail.com>
2020-02-12 13:17:19 -0500

    Initial commit, includes start of drive code

---
+++ src/main/java/frc/robot/commands/drive/CurvatureDrive.java
@@ -0,0 +1,58 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands.drive;
+
+import frc.robot.subsystems.Drivetrain;
+
+import java.util.function.DoubleSupplier;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+/**
+ * An example command that uses an example subsystem.
+ */
+public class CurvatureDrive extends CommandBase {
+  @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+  private final Drivetrain drivetrain;
+  private final DoubleSupplier yForward, zRotation;
+
+  /**
+   * Creates a new ExampleCommand.
+   *
+   * @param subsystem The subsystem used by this command.
+   */
+  public CurvatureDrive(Drivetrain subsystem, DoubleSupplier forward, DoubleSupplier rotation) {
+    drivetrain = subsystem;
+    yForward = forward;
+    zRotation = rotation;
+    addRequirements(drivetrain);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    drivetrain.curvatureDrive(yForward.getAsDouble(), zRotation.getAsDouble(), true);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    drivetrain.stopDrive();
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
